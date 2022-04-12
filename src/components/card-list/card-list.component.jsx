c7824707c397a9aa924739515decca2db0421ec5
lguilarte <leomiguel1907@gmail.com>
2022-04-11 20:54:35 -0400

    Card list component and styles

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,24 @@
 import React,{ Component} from 'react';
-
+import Card from '../card/card.component';
 class CardList extends Component {
     render(){
         const {monsters} =  this.props;
         
         return(
             <div>
-                {monsters.map((monster) => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+                {monsters.map((monster) => {
+                    const {name, email, id} = monster;
+                    return (
+                        <div className='card-container' key={id}>
+                            <img 
+                                alt={`monster ${name}`} 
+                                src={`https://robohash.org/${id}?set=set2&size=180x180`} 
+                            />                        
+                            <h2>{name}</h2>                        
+                            <p>{email}</p>
+                        </div>
+                    );
+                })}
             </div>            
         );
     }
